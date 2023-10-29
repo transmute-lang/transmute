96f2087ab3b9194cc2bbefaf7ce26a40a8120c29
Christophe Pollet <cpollet@users.noreply.github.com>
2023-10-29 22:04:31 +0100

    Add cov.sh

---
+++ cov.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# https://lib.rs/crates/cargo-llvm-cov
+
+source <(cargo llvm-cov show-env --export-prefix)
+
+cargo llvm-cov clean --workspace
+cargo build
+cargo test
+cargo llvm-cov report --html --hide-instantiations
