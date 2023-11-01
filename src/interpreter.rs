1664daf2c2285206f2bea034c31f11f09517cf0c
Christophe Pollet <cpollet@users.noreply.github.com>
2023-11-01 20:50:14 +0100

    Add fibonacci iter test

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -343,6 +343,26 @@ mod tests {
         }
         f(9) + 8;
     "# => Number(42));
+    eval!(fibonacci_iter, r#"
+        let f(n) = {
+            if n == 0 { ret 0; }
+            if n == 1 { ret 1; }
+
+            let prev_prev = 0;
+            let prev = 1;
+            let current = 0;
+
+            while n > 1 {
+                current = prev_prev + prev;
+                prev_prev = prev;
+                prev = current;
+                n = n - 1;
+            }
+
+            current;
+        }
+        f(9) + 8;
+    "# => Number(42));
     eval!(wtf, r#"
         let wtf(i, j) = {
             ret if i > j {
